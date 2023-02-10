e5def748bdf999879c62b70e63fa642e2a5b6fc9
Sudhanva CR <crs.holla@gmail.com>
2023-02-10 21:43:49 +0530

    Update the Server URL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://openai-t83z.onrender.com', {
+    const response = await fetch('https://codex-oxqn.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
